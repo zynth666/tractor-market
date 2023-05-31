22bcd79a8d7883e976f896144c239a3547c6dc28
Maurice Wessely <maurice.wessely19@gmail.com>
2023-05-31 18:32:53 +0200

    Add marketpage

---
+++ Helpers/ImagePathHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+
+namespace TractorMarket.Helpers
+{
+    class ImagePathHelper : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is string imageName)
+            {
+                string imagePath = $"pack://application:,,,/Assets/Images/Tractors/{imageName}.jpg";
+                return imagePath;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
