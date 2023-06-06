f9a182c7abeb2da80ec9549f458f84d2802b0d50
Dennis Eisenfeld <dennis.eisenfeld@yahoo.de>
2023-06-06 16:54:35 +0200

    Make Budget prop reactive

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,15 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using TractorMarket.Helpers;
 using TractorMarket.Models;
 
 namespace TractorMarket.Entities;
 
-public class User
+public partial class User : ObservableObject
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public long Budget { get; set; }
+    
+    [ObservableProperty]
+    public long _budget;
     public bool IsAdmin { get; set; }
     
     [NotMapped]
