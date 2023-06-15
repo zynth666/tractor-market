f486c05f0536c58049a096acfab120da63dc1763
Maurice Wessely <maurice.wessely19@gmail.com>
2023-06-15 02:29:07 +0200

    Add filters

--- Services/TractorService.cs
+++ Services/TractorService.cs
@@ -21,6 +21,14 @@ public class TractorService
             .ToList();
     }
 
+    public List<Tractor> GetFilteredTractorsForCustomers(int min_eur_in, int max_eur_in, int min_kmh_in, int max_kmh_in, int min_ps_in, int max_ps_in, int min_year_in, int max_year_in)
+    {
+        return _context.Tractors
+            .Where(tractor => tractor.Stock > 0 && min_eur_in <= tractor.Price && max_eur_in >= tractor.Price && min_kmh_in <= tractor.Velocity && max_kmh_in >= tractor.Velocity && min_ps_in <= tractor.Horsepower && max_ps_in >= tractor.Horsepower && min_year_in <= tractor.Vintage && max_year_in >= tractor.Vintage)
+            .ToList();
+    }
+
+
     public List<Tractor> GetTractorsForAdmin()
     {
         return _context.Tractors
