f6c87384eaa2f2d4d78744e3c7441b05318cbe74
Dennis Eisenfeld <dennis.eisenfeld@yahoo.de>
2023-06-18 22:48:54 +0200

    Remove unnecessary import

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
-using TractorMarket.Helpers;
 using TractorMarket.Services;
 using TractorMarket.Views.Pages;
 using Wpf.Ui.Common.Interfaces;
